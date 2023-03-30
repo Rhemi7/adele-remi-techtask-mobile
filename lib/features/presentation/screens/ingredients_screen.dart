99d761b1c0ca98fc45457fe28a826d133936d4ec
aderemilekun <adelefatai@yahoo.com>
2023-03-31 00:52:05 +0100

    Implemented Ingredients screen

---
+++ lib/features/presentation/screens/ingredients_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:tech_task/features/presentation/notifier/ingredients_notifier/get_ingredients_state.dart';
+import 'package:tech_task/features/presentation/provider/providers.dart';
+import 'package:tech_task/utils/margin.dart';
+
+import '../../../utils/resolution.dart';
+import '../widgets/button.dart';
+
+class IngredientsScreen extends ConsumerStatefulWidget {
+  const IngredientsScreen({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<IngredientsScreen> createState() => _IngredientsScreenState();
+}
+
+class _IngredientsScreenState extends ConsumerState<IngredientsScreen> {
+  List<bool> values = [];
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref
+          .watch(getIngredientsNotifierProvider.notifier)
+          .getFridgeIngredient()
+          .then((value) {
+        for (var item
+            in ref.watch(getIngredientsNotifierProvider.notifier).items) {
+          values.add(false);
+        }
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Consumer(
+          builder: (BuildContext context, WidgetRef ref, Widget? child) {
+            final ingredientsState = ref.watch(getIngredientsNotifierProvider);
+            if (ingredientsState is GetIngredientsLoading) {
+              return Center(child: const CircularProgressIndicator());
+            } else if (ingredientsState is GetIngredientsLoaded) {
+              return Column(
+                children: [
+                  Expanded(
+                      child: ListView.separated(
+                        padding: EdgeInsets.zero,
+                          itemBuilder: (BuildContext cxt, int i) {
+                            return CheckboxListTile(
+                              value: values[i],
+                              onChanged: (val) {
+                                values[i] = !values[i];
+                                setState(() {});
+                              },
+                              title: Text(ingredientsState.ingredients[i].title!),
+                            );
+                          },
+                          separatorBuilder: (context, index) {
+                            return YMargin(10);
+                          },
+                          itemCount: ingredientsState.ingredients.length)),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+                    child: PrimaryButton(
+                      color: Colors.red,
+                      width: Resolution.screenWidth(context),
+                      paddingHoriz: 25,
+                      inActiveButtonColor: Colors.red,
+                      text: "Continue",
+                    ),
+                  )
+                ],
+              );
+            } else if (ingredientsState is GetIngredientsError) {
+              return Text(ingredientsState.message);
+            }
+            return const SizedBox.shrink();
+          },
+        ),
+      ),
+    );
+  }
+}
