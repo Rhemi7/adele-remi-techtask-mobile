16d1fa2a9b47bfee1bd8ddff069b35e1982f4254
aderemilekun <adelefatai@yahoo.com>
2023-03-30 18:19:14 +0100

    Get Ingredient model fromJson test passed

--- test/features/data/model/ingredient_model_test.dart
+++ test/features/data/model/ingredient_model_test.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tech_task/features/data/model/ingredient_model.dart';
+
+import '../../../data/data_reader.dart';
 
 main() {
 
@@ -20,5 +24,16 @@ main() {
 
   test("should be a subclass of Ingredient response", () {
     expect(testIngredientModel, isA<List<Ingredient>>());
+  });
+
+  group('fromJson', () {
+    test(
+      'should return a valid model when fromJson is called',
+          () async {
+        final jsonString = dataReader('ingredient_response.json');
+        final result = ingredientFromJson(jsonString);
+        expect(result, testIngredientModel);
+      },
+    );
   });
 }
