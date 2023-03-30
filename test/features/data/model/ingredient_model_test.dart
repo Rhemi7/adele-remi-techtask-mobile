1f4e25dc00830a0f9672e9cc13911389ba82e0e9
aderemilekun <adelefatai@yahoo.com>
2023-03-30 18:49:14 +0100

    Get Ingredient model toJson test passed

--- test/features/data/model/ingredient_model_test.dart
+++ test/features/data/model/ingredient_model_test.dart
@@ -6,20 +6,10 @@ import 'package:tech_task/features/data/model/ingredient_model.dart';
 import '../../../data/data_reader.dart';
 
 main() {
-
   final testIngredientModel = [
-    Ingredient(
-        title: "Ham",
-        useBy: DateTime.parse("2020-11-25")
-    ),
-    Ingredient(
-        title: "Cheese",
-        useBy: DateTime.parse("2020-01-08")
-    )
-    ,Ingredient(
-        title: "Bread",
-        useBy: DateTime.parse("2020-11-01")
-    ),
+    Ingredient(title: "Ham", useBy: DateTime.parse("2020-11-25")),
+    Ingredient(title: "Cheese", useBy: DateTime.parse("2020-01-08")),
+    Ingredient(title: "Bread", useBy: DateTime.parse("2020-11-01")),
   ];
 
   test("should be a subclass of Ingredient response", () {
@@ -29,10 +19,27 @@ main() {
   group('fromJson', () {
     test(
       'should return a valid model when fromJson is called',
-          () async {
+      () async {
         final jsonString = dataReader('ingredient_response.json');
         final result = ingredientFromJson(jsonString);
         expect(result, testIngredientModel);
+      },
+    );
+  });
+
+  group('toJson', () {
+    test(
+      'should return a JSON map containing the proper data',
+      () async {
+        // act
+        final result = ingredientToJson(testIngredientModel);
+        // assert
+        final expectedMap = jsonEncode([
+          {"title": "Ham", "use-by": "2020-11-25"},
+          {"title": "Cheese", "use-by": "2020-01-08"},
+          {"title": "Bread", "use-by": "2020-11-01"}
+        ]);
+        expect(result, expectedMap);
       },
     );
   });
