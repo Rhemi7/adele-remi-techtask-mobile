add3ed75d1203a8a3b3de6db10eb29d7836e7e83
aderemilekun <adelefatai@yahoo.com>
2023-03-30 18:02:25 +0100

    Get Ingredient Remote data source test passed

---
+++ test/features/data/datasource/app_remote_data_source_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
+import 'package:tech_task/constants/constants.dart';
+import 'package:tech_task/core/error/failure.dart';
+import 'package:tech_task/features/data/datasources/app_remote_data_source.dart';
+import 'package:tech_task/features/data/model/ingredient_model.dart';
+import '../../../data/data_reader.dart';
+
+class MockClient extends Mock implements http.Client {}
+
+void main() {
+  late MockClient client;
+  late AppRemoteDataSourceImpl dataSource;
+
+  var ingUrl = Uri.parse("${Constant.baseUrl}/ingredients/");
+
+  setUp(() {
+    client = MockClient();
+    dataSource = AppRemoteDataSourceImpl(client);
+  });
+
+
+  group("Get Ingredients", () {
+    test("Returns an Ingredient Response", () async {
+      when(client.get(ingUrl)).thenAnswer((_) async => http.Response(dataReader("ingredient_response.json"), 200));
+
+      expect(await dataSource.getIngredients(), isA<List<Ingredient>>());
+    });
+  });
+}
