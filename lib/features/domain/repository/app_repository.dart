807b4ad74e15c1def80779d4b1650152b5c4345a
aderemilekun <adelefatai@yahoo.com>
2023-03-30 20:18:34 +0100

    Get recipes repository

--- lib/features/domain/repository/app_repository.dart
+++ lib/features/domain/repository/app_repository.dart
@@ -2,7 +2,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:tech_task/core/error/failure.dart';
 import 'package:tech_task/features/data/model/ingredient_model.dart';
+import 'package:tech_task/features/data/model/recipe_model.dart';
 
 abstract class AppRepository {
   Future<Either<Failure, List<Ingredient>>> getIngredients();
+  Future<Either<Failure, List<Recipe>>> getRecipes(String ingredients);
 }
