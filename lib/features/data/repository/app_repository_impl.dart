e75c05905ffd56b1689d19d4ff92695d3fbb493c
aderemilekun <adelefatai@yahoo.com>
2023-03-30 20:14:43 +0100

    Repository implementation for get recipes

--- lib/features/data/repository/app_repository_impl.dart
+++ lib/features/data/repository/app_repository_impl.dart
@@ -3,6 +3,7 @@ import 'package:dartz/dartz.dart';
 import 'package:tech_task/core/error/failure.dart';
 import 'package:tech_task/features/data/datasources/app_remote_data_source.dart';
 import 'package:tech_task/features/data/model/ingredient_model.dart';
+import 'package:tech_task/features/data/model/recipe_model.dart';
 import 'package:tech_task/features/domain/repository/app_repository.dart';
 
 class AppRepositoryImpl implements AppRepository {
@@ -14,6 +15,16 @@ class AppRepositoryImpl implements AppRepository {
     try {
       final remoteIngredients = await dataSource.getIngredients();
       return Right(remoteIngredients);
+    } on ServerException {
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Recipe>>> getRecipes(String ingredients) async {
+    try {
+      final remoteRecipes = await dataSource.getRecipes(ingredients);
+      return Right(remoteRecipes);
     } on ServerException {
       return Left(ServerFailure());
     }
