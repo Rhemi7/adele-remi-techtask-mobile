6fbdef065f96c1866468ad99c75466103b686b62
aderemilekun <adelefatai@yahoo.com>
2023-03-30 21:44:02 +0100

    Recipe model fromJson test passed

--- test/features/data/model/recipes_model_test.dart
+++ test/features/data/model/recipes_model_test.dart
@@ -1,10 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tech_task/features/data/model/recipe_model.dart';
 
+import '../../../data/data_reader.dart';
 import '../../../data/test_recipe_model.dart';
 
 main() {
   test("should be a subclass of Recipes response", () {
     expect(testRecipeModel, isA<List<Recipe>>());
+  });
+
+  group('fromJson', () {
+    test(
+      'should return a valid recipe model when fromJson is called',
+          () async {
+        final jsonString = dataReader('recipe_response.json');
+        final result = recipeFromJson(jsonString);
+        expect(result, testRecipeModel);
+      },
+    );
   });
 }
