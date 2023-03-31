29ae3d484ad89d367d50a3857bc61783f0af6163
aderemilekun <adelefatai@yahoo.com>
2023-03-31 02:44:49 +0100

    Implemented Recipe Screen

---
+++ lib/features/presentation/screens/recipe_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:tech_task/features/presentation/notifier/recipes_notifier/get_recipes_state.dart';
+import 'package:tech_task/utils/margin.dart';
+
+import '../provider/providers.dart';
+
+class RecipeScreen extends ConsumerStatefulWidget {
+  final String ingredients;
+  const RecipeScreen({Key? key, required this.ingredients}) : super(key: key);
+
+  @override
+  ConsumerState<RecipeScreen> createState() => _RecipeScreenState();
+}
+
+class _RecipeScreenState extends ConsumerState<RecipeScreen> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref
+          .watch(getRecipesNotifierProvider.notifier)
+          .getFoodRecipes(widget.ingredients);
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Consumer(
+          builder: (BuildContext context, WidgetRef ref, Widget? child) {
+            final recipeState = ref.watch(getRecipesNotifierProvider);
+            if (recipeState is GetRecipesLoading) {
+              return Center(child: const CircularProgressIndicator());
+            } else if (recipeState is GetRecipesLoaded) {
+              var recipes = recipeState.recipes;
+              return SingleChildScrollView(
+                child: Column(
+                  children: recipes!.map((recipe) => Container(
+                    height: 110,
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 12.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              recipe.title!,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20.0),
+                            ),
+                            YMargin(5),
+                            Text(
+                              "Recipes",
+                              style: TextStyle(
+                                  fontStyle: FontStyle.italic,
+                                  color: Colors.red,
+                                  fontSize: 12),
+                            ),
+                            Expanded(
+                              child: GridView.builder(
+                                gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                                  // spacing on the vertical axis
+                                  // mainAxisSpacing: 2,
+                                  crossAxisCount: 3,
+                                  childAspectRatio: 2.7 / 0.5,
+                                ),
+                                itemBuilder: (cxt, index) {
+                                  return Text(recipe.recipeIngredients![index]);
+                                },
+                                itemCount: recipe.recipeIngredients?.length,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )).toList(),
+                ),
+              );
+            } else if (recipeState is GetRecipesError) {
+              return Text(recipeState.message);
+            }
+            return const SizedBox.shrink();
+          },
+        ),
+      ),
+    );
+  }
+}
