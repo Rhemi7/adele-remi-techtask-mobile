48a1ebd2cfcf9c6519833029c573291b5605a81c
aderemilekun <adelefatai@yahoo.com>
2023-03-31 01:37:54 +0100

    Checking expiry date and displaying an alert dialog

--- lib/features/presentation/screens/ingredients_screen.dart
+++ lib/features/presentation/screens/ingredients_screen.dart
@@ -53,17 +53,29 @@ class _IngredientsScreenState extends ConsumerState<IngredientsScreen> {
                           return CheckboxListTile(
                             value: values[i],
                             onChanged: (val) {
-                              values[i] = !values[i];
-                              if (values[i]) {
-                                nameIngredients.add(
-                                    ingredientsState.ingredients[i].title!);
+                              if (DateTime.now().isAfter(
+                                  ingredientsState.ingredients[i].useBy!) && values[i] == false) {
+                                showDialog(
+                                    context: context,
+                                    builder: (builder) => AlertDialog(
+                                      title: Text("Cannot be Added"),
+                                      content: Text(
+                                          '${ingredientsState.ingredients[i].title!} has passed it\'s expiry date and cannot be added. \n \nIf you still wish to select it, please long press the tile.'),
+                                    ));
                               } else {
-                                nameIngredients.remove(
-                                    ingredientsState.ingredients[i].title!);
+                                values[i] = !values[i];
+                                if (values[i]) {
+                                  nameIngredients.add(ingredientsState
+                                      .ingredients[i].title!);
+                                } else {
+                                  nameIngredients.remove(ingredientsState
+                                      .ingredients[i].title!);
+                                }
+                                setState(() {});
                               }
-                              setState(() {});
                             },
-                            title: Text(ingredientsState.ingredients[i].title!),
+                            title: Text(
+                                ingredientsState.ingredients[i].title!),
                           );
                         },
                         separatorBuilder: (context, index) {
